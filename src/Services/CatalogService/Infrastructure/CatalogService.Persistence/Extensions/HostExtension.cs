7827af8bf77b4fd3172d66ea7963a8621df44392
MrBuluc <hakkicanbuluc@gmail.com>
2024-08-26 10:01:40 +0300

    [Feat] CatalogService.Persistence HostExtension

---
+++ src/Services/CatalogService/Infrastructure/CatalogService.Persistence/Extensions/HostExtension.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Polly;
+
+namespace CatalogService.Persistence.Extensions
+{
+    public static class HostExtension
+    {
+        public static IWebHost MigrateDbContext<TContext>(this IWebHost host, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                IServiceProvider services = scope.ServiceProvider;
+
+                ILogger<TContext> logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Migrating db associated with context {DbContextName}", typeof(TContext).Name);
+
+                    Policy.Handle<SqlException>()
+                        .WaitAndRetry(new TimeSpan[]
+                        {
+                            TimeSpan.FromSeconds(3),
+                            TimeSpan.FromSeconds(5),
+                            TimeSpan.FromSeconds(8),
+                        })
+                        .Execute(() => InvokeSeeder<TContext>(seeder, services.GetService<TContext>(), services));
+
+                    logger.LogInformation("Migrated db associated with context {DbContextName}", typeof(TContext).Name);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred while migrating the db used on context {DbContextName}", typeof(TContext).Name);
+                }
+            }
+
+            return host;
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext? context, IServiceProvider services) where TContext : DbContext
+        {
+            if (context is not null)
+            {
+                context.Database.EnsureCreated();
+                context.Database.Migrate();
+                seeder(context, services);
+            }
+
+        }
+    }
+}
