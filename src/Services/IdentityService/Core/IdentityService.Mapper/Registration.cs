e21754f497ec1f4c319dcf9aefb7173c3f191648
MrBuluc <hakkicanbuluc@gmail.com>
2024-09-03 15:12:10 +0300

    [Feat] IdentityService.Mapper

---
+++ src/Services/IdentityService/Core/IdentityService.Mapper/Registration.cs
@@ -0,0 +1,13 @@
+﻿using IdentityService.Application.Interfaces.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityService.Mapper
+{
+    public static class Registration
+    {
+        public static void AddCustomMapper(this IServiceCollection services)
+        {
+            services.AddSingleton<IMapper, AutoMapper.Mapper>();
+        }
+    }
+}
