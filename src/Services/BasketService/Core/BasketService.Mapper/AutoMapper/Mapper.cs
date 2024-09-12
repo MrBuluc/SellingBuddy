6ba7642fbdf602d3b19237686ed7f79c9e9c2aee
MrBuluc <hakkicanbuluc@gmail.com>
2024-09-12 11:43:42 +0300

    [Feat] BasketService.Mapper

---
+++ src/Services/BasketService/Core/BasketService.Mapper/AutoMapper/Mapper.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using AutoMapper.Internal;
+
+namespace BasketService.Mapper.AutoMapper
+{
+    public class Mapper : Application.Interfaces.AutoMapper.IMapper
+    {
+        public static List<TypePair> typePairs = [];
+        private IMapper MapperContainer;
+
+        protected void Config<TDestination, TSource>(int depth = 5, string? ignore = null)
+        {
+            TypePair typePair = new(typeof(TSource), typeof(TDestination));
+
+            if (typePairs.Any(tp => tp.DestinationType == typePair.DestinationType && tp.SourceType == typePair.SourceType) && ignore is null)
+            {
+                return;
+            }
+
+            typePairs.Add(typePair);
+            MapperConfiguration configuration = new(cfg =>
+            {
+                foreach (TypePair item in typePairs)
+                {
+                    if (ignore is not null)
+                    {
+                        cfg.CreateMap(item.SourceType, item.DestinationType)
+                        .MaxDepth(depth)
+                        .ForMember(ignore, member => member.Ignore())
+                        .ReverseMap();
+                    }
+                    else
+                    {
+                        cfg.CreateMap(item.SourceType, item.DestinationType)
+                        .MaxDepth(depth)
+                        .ReverseMap();
+                    }
+                }
+            });
+
+            MapperContainer = configuration.CreateMapper();
+        }
+
+        public TDestination Map<TDestination, TSource>(TSource source, string? ignore = null)
+        {
+            Config<TDestination, TSource>(ignore: ignore);
+            return MapperContainer.Map<TSource, TDestination>(source);
+        }
+
+        public IList<TDestination> Map<TDestination, TSource>(IList<TSource> source, string? ignore = null)
+        {
+            Config<TDestination, TSource>(ignore: ignore);
+            return MapperContainer.Map<IList<TSource>, IList<TDestination>>(source);
+        }
+
+        public TDestination Map<TDestination>(object source, string? ignore = null)
+        {
+            Config<TDestination, object>(ignore: ignore);
+            return MapperContainer.Map<TDestination>(source);
+        }
+
+        public IList<TDestination> Map<TDestination>(IList<object> source, string? ignore = null)
+        {
+            Config<TDestination, IList<object>>(ignore: ignore);
+            return MapperContainer.Map<IList<TDestination>>(source);
+        }
+    }
+}
---
+++ src/Services/BasketService/Core/BasketService.Mapper/Registration.cs
@@ -0,0 +1,13 @@
+﻿using BasketService.Application.Interfaces.AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BasketService.Mapper
+{
+    public static class Registration
+    {
+        public static void AddCustomMapper(this IServiceCollection services)
+        {
+            services.AddSingleton<IMapper, AutoMapper.Mapper>();
+        }
+    }
+}
